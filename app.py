7d35f115c9e6958f7eb3676f8a95a26761d5bdb2
n-kirankuamr <nkiran.appdev@gmail.com>
2025-10-14 11:14:59 +0530

    changed app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, request
 
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"message": "Welcome to Flask with Docker and VS Code!"})
+
 
 @app.route('/user', methods=['POST'])
 def create_user():
@@ -13,6 +16,7 @@ def create_user():
         "status": "User created successfully",
         "user": data
     }), 201
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
